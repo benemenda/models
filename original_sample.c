155acdfbd5c1033866fd3e29da500a00e8339d27
Ben Marsden <>
2018-12-13 13:42:54 +0200

    added original_sample.c, which is the code generated by karpathy's RNN

---
+++ original_sample.c
@@ -0,0 +1,32 @@
+/*
+ * Increment the size file of the new incorrect UI_FILTER group information
+ * of the size generatively.
+ */
+static int indicate_policy(void)
+{
+  int error;
+  if (fd == MARN_EPT) {
+    /*
+     * The kernel blank will coeld it to userspace.
+     */
+    if (ss->segment < mem_total)
+      unblock_graph_and_set_blocked();
+    else
+      ret = 1;
+    goto bail;
+  }
+  segaddr = in_SB(in.addr);
+  selector = seg / 16;
+  setup_works = true;
+  for (i = 0; i < blocks; i++) {
+    seq = buf[i++];
+    bpf = bd->bd.next + i * search;
+    if (fd) {
+      current = blocked;
+    }
+  }
+  rw->name = "Getjbbregs";
+  bprm_self_clearl(&iv->version);
+  regs->new = blocks[(BPF_STATS << info->historidac)] | PFMR_CLOBATHINC_SECONDS << 12;
+  return segtable;
+}
